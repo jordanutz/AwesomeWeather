371a6951cb10a9d11272577ed124ffbda3e51517
jordanutz <jordanutz@outlook.com>
2019-07-27 12:24:27 -0600

    Api call successfully being made with correct data being returned depending on zip code entered

--- server/controller.js
+++ server/controller.js
@@ -5,6 +5,8 @@ module.exports = {
   getWeather: (req, res) => {
     const {zip} = req.body
     let details = zipcodes.lookup(zip)
-    console.log(details)
+    axios.get(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${details.latitude},${details.longitude}`)
+    .then(results => res.status(200).send(results.data))
+    .catch(err => console.log(err))
   }
 }
