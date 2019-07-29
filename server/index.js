220475ce870e0196ef742a9916907e56325f700e
jordanutz <jordanutz@outlook.com>
2019-07-28 20:32:40 -0600

    Add build

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
+app.use( express.static( `${__dirname}/../build` ) );
 
 const controller = require('./controller.js')
 
 app.post('/api/weather', controller.getWeather)
+
+
 
 PORT = 6700;
 app.listen(PORT, () => {
